ac72bb5926961111288ceb96accef5de4228bd79
Maf'j Alvarez <1101918+blujay@users.noreply.github.com>
2025-11-21 19:02:25 +0000

    Created dynamic templates for uploading default parameters and local authority costs.

--- app/routes/baseline.js
+++ app/routes/baseline.js
@@ -11,4 +11,35 @@ router.get("/baseline", function (request, response) {
 // response.render(version + '/my-page')
 
 
+// ----------------------------------
+// Routing user intent
+//-----------------------------------
+
+router.get("/settings/manage-default-parameters", function (req, res) {
+  // Get the intent from query string or fallback to default
+  const userIntent = req.query['user-intent'] || "manage-default-parameters";
+  req.session.data.userIntent = userIntent;
+  res.render('/' + version + "/settings/manage-default-parameters");
+});
+
+router.get("/settings/manage-la-disposal-costs", function (req, res) {
+  // Get the intent from query string or fallback to default
+  const userIntent = req.query['user-intent'] || "manage-la-disposal-costs";
+  req.session.data.userIntent = userIntent;
+  res.render('/' + version + "/settings/manage-la-disposal-costs");
+});
+
+
+// Logging session data  
+  
+  router.use((req, res, next) => {    
+      const log = {  
+        method: req.method,  
+        url: req.originalUrl,  
+        data: req.session.data  
+      }  
+      console.log(JSON.stringify(log, null, 2))  
+     
+    next()  
+  })  
 module.exports = router
