336ba15dd84428219c1dd776257d1bbb95e0373c
Maf'j Alvarez <1101918+blujay@users.noreply.github.com>
2026-01-08 15:55:00 +0000

    adding error handling for run name variations

--- app/filters.js
+++ app/filters.js
@@ -7,4 +7,8 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
-
+addFilter('regexMatch', function(str, pattern) {
+  if (!str) return false;
+  const regex = new RegExp(pattern);
+  return regex.test(str);
+})
